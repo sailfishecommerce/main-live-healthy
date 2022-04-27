efae1b87b268c8bf8e0c2ff00b24646c33dd4024
BisiDavi <oludavidconnect@gmail.com>
2022-04-27 12:39:33 +0100

    password reset page updated

---
+++ components/AccountRecovery/PasswordReset.tsx
@@ -0,0 +1,88 @@
+/* eslint-disable no-console */
+import { Formik } from 'formik'
+import Image from 'next/image'
+import { useRouter } from 'next/router'
+
+import { displayFormElement } from '@/components/Form/FormElement'
+import { forgotPasswordSchema } from '@/components/Form/schema/AuthSchema'
+import { useAccount, useToast } from '@/hooks'
+import passwordResetForm from '@/json/password-reset.json'
+
+export default function PasswordReset() {
+  const router = useRouter()
+  const { recoverPassword } = useAccount()
+  const { isLoading, isSuccessful, hasError } = useToast()
+  const searchParams = router.query
+  const key: any | string = searchParams.key
+
+  return (
+    <div className="container flex flex-col lg:flex-row items-center mx-auto py-4 py-lg-5 my-4">
+      <div className="row lg:w-1/2 w-full justify-center">
+        <div className="col-lg-8 col-md-10">
+          <h2 className="h3 mb-4">Forgot your password?</h2>
+          <p className="fs-md">
+            Reset your password, ensure to fill details appropriately
+          </p>
+          <div className="card py-2 mt-4">
+            <Formik
+              initialValues={{
+                newPassword: '',
+                confirmNewPassword: '',
+              }}
+              validationSchema={forgotPasswordSchema}
+              onSubmit={(values, { setSubmitting, resetForm }) => {
+                const loading = isLoading()
+                recoverPassword(values.newPassword, key)
+                  .then((response) => {
+                    if (response?.success) {
+                      isSuccessful(loading, 'Password reset successful')
+                      router.push('/my-account')
+                    } else {
+                      hasError(loading, 'error resetting password')
+                    }
+                  })
+                  .catch((err) => {
+                    console.log('error', err)
+                    hasError(loading, 'error resetting password')
+                  })
+                resetForm()
+                setSubmitting(false)
+              }}
+            >
+              {(formik) => (
+                <form
+                  noValidate
+                  className="card-body needs-validation"
+                  onSubmit={formik.handleSubmit}
+                >
+                  <div className="row">
+                    {passwordResetForm.reset.map((formInput) => (
+                      <div className="col-12" key={formInput.name}>
+                        {displayFormElement(formInput, formik)}
+                      </div>
+                    ))}
+                  </div>
+                  <button
+                    aria-label="password reset"
+                    className="btn btn-primary"
+                    type="submit"
+                  >
+                    Submit
+                  </button>
+                </form>
+              )}
+            </Formik>
+          </div>
+        </div>
+      </div>
+      <div className="lg:w-1/2 w-full image-wrapper">
+        <Image
+          src="/forgotPassword.webp"
+          alt="forgot password"
+          height={500}
+          width={700}
+        />
+      </div>
+    </div>
+  )
+}
--- components/Blog/BlogAside.tsx
+++ components/Blog/BlogAside.tsx
@@ -3,7 +3,7 @@ import TableofContent from '@/components/Blog/TableofContent'
 
 export default function BlogAside() {
   return (
-    <aside className="w-1/4 hidden md:lg mt-52 sticky top-24 z-30 right-24">
+    <aside className="w-1/4 hidden lg:flex lg:flex-col mt-52 sticky top-24 z-30 right-24">
       <TableofContent />
       <div className="mb-14" />
       <PopularArticles />

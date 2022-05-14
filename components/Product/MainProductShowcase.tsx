4a1c8e6b4ddc7460a5a411362c10b913c2666371
BisiDavi <oludavidconnect@gmail.com>
2022-05-14 17:34:03 +0100

    pages updated

--- components/Product/MainProductShowcase.tsx
+++ components/Product/MainProductShowcase.tsx
@@ -2,7 +2,7 @@ import classNames from 'classnames'
 import { Configure, Index } from 'react-instantsearch-dom'
 
 import { Container } from '@/components/Container'
-import LazyLoader from '@/components/Loader/LazyLoader'
+// import LazyLoader from '@/components/Loader/LazyLoader'
 import InfiniteHitsSlider from '@/components/Slider/InfiniteHitSlider'
 import { indexName as defaultIndexName } from '@/utils/env'
 
@@ -24,26 +24,26 @@ export default function MainProductShowcase({
   ...searchParameters
 }: ProductsShowcaseProps) {
   return (
-    <LazyLoader height={450} mobileHeight={320}>
-      <Index indexName={indexName} indexId={indexId}>
-        <Configure
-          enablePersonalization={true}
-          hitsPerPage={15}
-          {...searchParameters}
-        />
-        <section
-          className={classNames('py-4 lg:py-10 container mx-auto', className)}
-        >
-          <Container>
-            {title && (
-              <h4 className="text-2xl font-bold  mb-2 lg:mb-4 lg:ml-3">
-                {title}
-              </h4>
-            )}
-            <InfiniteHitsSlider tabColor={tabColor} />
-          </Container>
-        </section>
-      </Index>
-    </LazyLoader>
+    // <LazyLoader height={450} mobileHeight={320}>
+    <Index indexName={indexName} indexId={indexId}>
+      <Configure
+        enablePersonalization={true}
+        hitsPerPage={15}
+        {...searchParameters}
+      />
+      <section
+        className={classNames('py-4 lg:py-10 container mx-auto', className)}
+      >
+        <Container>
+          {title && (
+            <h4 className="text-2xl font-bold  mb-2 lg:mb-4 lg:ml-3">
+              {title}
+            </h4>
+          )}
+          <InfiniteHitsSlider tabColor={tabColor} />
+        </Container>
+      </section>
+    </Index>
+    // </LazyLoader>
   )
 }

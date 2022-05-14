180d37a9ddb05873c4abb22786e03f05b6d7ad08
BisiDavi <oludavidconnect@gmail.com>
2022-05-15 00:33:55 +0100

    page updated

--- components/Slider/AliceSlider/ProductSlider.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import AliceCarousel from 'react-alice-carousel'
-import 'react-alice-carousel/lib/alice-carousel.css'
-
-const handleDragStart = (e: any) => e.preventDefault()
-
-export default function ProductSlider() {
-  return <div>ProductSlider</div>
-}

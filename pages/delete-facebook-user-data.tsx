309fee5b16ad1e4b51e9043d0464f89a026fe3cc
BisiDavi <oludavidconnect@gmail.com>
2022-05-18 16:16:34 +0100

    facebook social login

---
+++ pages/delete-facebook-user-data.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import Applayout from '@/layouts/app-layout'
+
+export default function DeletefacebookUserData() {
+  return (
+    <Applayout title="Delete facebook user data">
+      <h1>To delete facebook user data</h1>
+    </Applayout>
+  )
+}

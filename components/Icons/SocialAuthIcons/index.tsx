3ddbada85aac4ef4fc68ec4bd91f4bf1b2b4f812
BisiDavi <oludavidconnect@gmail.com>
2022-05-20 04:45:55 +0100

    social login updated

--- components/Icons/SocialAuthIcons/index.tsx
+++ components/Icons/SocialAuthIcons/index.tsx
@@ -1,12 +1,14 @@
 import FacebookIconFill from '@/components/Icons/FacebookIconFill'
 import GoogleFillIcon from '@/components/Icons/GoogleFillIcon'
+import useFirebaseAuth from '@/hooks/useFirebaseAuth'
 import { FacebookButton } from '@/scripts/facebook-script'
 
 export default function Index() {
+  const { GoogleSignin } = useFirebaseAuth()
   return (
     <div className="social-button mt-4 flex items-center mx-auto justify-center">
       <FacebookButton />
-      <button type="button" className="mx-2">
+      <button type="button" className="mx-2" onClick={GoogleSignin}>
         <GoogleFillIcon />
       </button>
       <button type="button" className="mx-4">

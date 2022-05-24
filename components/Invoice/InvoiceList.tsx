97c03959ea8d984c266e078d1aac1f1189c84cbe
BisiDavi <oludavidconnect@gmail.com>
2022-05-24 05:44:08 +0100

    page updated

--- components/Invoice/InvoiceList.tsx
+++ components/Invoice/InvoiceList.tsx
@@ -19,42 +19,61 @@ function InvoiceHitComponent({
       : invoiceHit?.images[0].file.url
   const productPrice = currency === 'HKD' ? invoiceHit?.sale_price : price
   return (
-    <tr className="view">
-      <td>
-        <div className="product-view flex items-center">
-          {invoiceHit && (
-            <Image
-              src={productImage}
-              alt={invoiceHit?.name}
-              height={100}
-              width={100}
-            />
-          )}
-          <div className="content flex flex-col ml-2">
-            <h1 className="font-thin text-xl">{invoiceHit?.name}</h1>
-            <p className="font-thin text-lg">SKU {invoiceHit?.sku}</p>
+    <>
+      <tr className="view">
+        <td>
+          <div className="product-view flex items-center">
+            {invoiceHit && (
+              <Image
+                src={productImage}
+                alt={invoiceHit?.name}
+                height={80}
+                width={80}
+              />
+            )}
+            <div className="content flex flex-col ml-2">
+              <h1 className="font-thin text-lg">{invoiceHit?.name}</h1>
+              <p className="font-thin text-md">SKU {invoiceHit?.sku}</p>
+            </div>
           </div>
-        </div>
-      </td>
-      <td>
-        <div className="price flex flex-col">
-          {invoiceHit?.price && price === invoiceHit?.price && (
+        </td>
+        <td>
+          <div className="price flex flex-col">
+            {invoiceHit?.price && price === invoiceHit?.price && (
+              <FormattedPrice
+                price={invoiceHit?.price}
+                className="text-sm font-bold strike-through"
+                currency={currency}
+              />
+            )}
             <FormattedPrice
-              price={invoiceHit?.price}
-              className="strike-through"
               currency={currency}
+              price={productPrice}
+              className="text-sm font-bold"
             />
-          )}
-          <FormattedPrice currency={currency} price={productPrice} />
-        </div>
-      </td>
-      <td>
-        <p className="font-thin">{quantity}</p>
-      </td>
-      <td>
-        <FormattedPrice price={price_total} currency={currency} />
-      </td>
-    </tr>
+          </div>
+        </td>
+        <td>
+          <p className="font-thin">{quantity}</p>
+        </td>
+        <td>
+          <FormattedPrice
+            className="text-sm font-bold"
+            price={price_total}
+            currency={currency}
+          />
+        </td>
+      </tr>
+      <style jsx>
+        {`
+          .view {
+            border-bottom: 1px solid #e5e5e6;
+            margin: 10px 0px;
+            height: 120px;
+          }
+        `}
+      </style>
+    </>
   )
 }
 const InvoiceHit = connectHits<any, any>(InvoiceHitComponent)
--- components/Invoice/index.tsx
+++ components/Invoice/index.tsx
@@ -17,74 +17,83 @@ export default function Invoice({ invoice }: any) {
   const shippingMethod = getShippingMethod(invoice)
 
   return (
-    <div className="invoice-receipt">
-      <div className="row justify-between items-center">
-        <Logo className="w-1/2" />
-        <div className="invoice-date flex flex-col">
-          <h1 className="text-3xl">{invoice.number}</h1>
-          <h5 className="font-thin text-xl">
-            {formatOrderDate(invoice.date_created)}
-          </h5>
-        </div>
-      </div>
-      <div className="row details grid grid-cols-3">
-        <div className="shipping-address">
-          <h1 className="font-bold text-xl my-2">SHIPPING ADDRESS</h1>
-          <p className="font-thin">
-            {invoice?.shipping.address1},{invoice?.shipping.address2}
-          </p>
-          <p className="font-thin">
-            {invoice?.shipping.zip} {invoice?.shipping.city}{' '}
-            {invoice?.shipping.state}
-          </p>
-          <p className="font-thin">{getCountry(invoice?.shipping.country)}</p>
-        </div>
-        <div className="customer">
-          <h1 className="font-bold text-xl my-2">CUSTOMER</h1>
-          <p className="font-thin">{invoice.billing.name}</p>
-          <p className="font-thin">{invoice.billing.address1}</p>
-          <p className="font-thin">{invoice.billing.address2}</p>
-          <p className="font-thin">{`${invoice.billing.zip} ${invoice.billing.city}`}</p>
-          <p className="font-thin">
-            {invoice.billing.city} {getCountry(invoice.billing.country)}
-          </p>
-        </div>
-        <div className="group">
-          <div className="payment-method">
-            <h1 className="font-bold text-xl my-2">PAYMENT METHOD</h1>
-            <p className="font-thin">{paymentMethod}</p>
-          </div>
-          <div className="shipping-method">
-            <h1 className="font-bold text-xl my-2">SHIPPING METHOD</h1>
-            <p className="font-thin">{shippingMethod[0]?.name}</p>
-            <p className="font-thin">COVID-19 might cause delays</p>
+    <>
+      <div className="invoice-receipt mt-12 bg-white p-6 rounded-xl">
+        <div className="row flex justify-between mb-16 items-center">
+          <Logo className="w-1/2" />
+          <div className="invoice-date flex flex-col">
+            <h1 className="text-2xl font-bold">{invoice.number}</h1>
+            <h5 className="font-thin text-lg">
+              {formatOrderDate(invoice.date_created)}
+            </h5>
           </div>
         </div>
+        <div className="row details grid grid-cols-3">
+          <div className="shipping-address">
+            <h1 className="font-semibold text-lg my-2">SHIPPING ADDRESS</h1>
+            <p className="font-thin">
+              {invoice?.shipping.address1},{invoice?.shipping.address2}
+            </p>
+            <p className="font-thin">
+              {invoice?.shipping.zip} {invoice?.shipping.city}{' '}
+              {invoice?.shipping.state}
+            </p>
+            <p className="font-thin">{getCountry(invoice?.shipping.country)}</p>
+          </div>
+          <div className="customer">
+            <h1 className="font-semibold text-lg my-2">CUSTOMER</h1>
+            <p className="font-thin">{invoice.billing.name}</p>
+            <p className="font-thin">{invoice.billing.address1}</p>
+            <p className="font-thin">{invoice.billing.address2}</p>
+            <p className="font-thin">{`${invoice.billing.zip} ${invoice.billing.city}`}</p>
+            <p className="font-thin">
+              {invoice.billing.city} {getCountry(invoice.billing.country)}
+            </p>
+          </div>
+          <div className="group">
+            <div className="payment-method">
+              <h1 className="font-semibold text-lg my-2">PAYMENT METHOD</h1>
+              <p className="font-thin">{paymentMethod}</p>
+            </div>
+            <div className="shipping-method">
+              <h1 className="font-semibold text-lg my-2">SHIPPING METHOD</h1>
+              <p className="font-thin">{shippingMethod[0]?.name}</p>
+              <p className="font-thin">COVID-19 might cause delays</p>
+            </div>
+          </div>
+        </div>
+        <div className="row items mt-6">
+          <table>
+            <thead>
+              <tr>
+                <th>ITEMS</th>
+                <th>PRICE</th>
+                <th>QTY</th>
+                <th>ITEM TOTAL</th>
+              </tr>
+            </thead>
+            <tbody>
+              {invoice.items.map((item: any) => (
+                <InvoiceList
+                  key={item.id}
+                  productName={item.product_name}
+                  price={item.price}
+                  quantity={item.quantity}
+                  price_total={item.price_total}
+                  currency={invoice.currency}
+                />
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
-      <div className="row items">
-        <table>
-          <thead>
-            <tr>
-              <th>ITEMS</th>
-              <th>PRICE</th>
-              <th>QTY</th>
-              <th>ITEM TOTAL</th>
-            </tr>
-          </thead>
-          <tbody>
-            {invoice.items.map((item: any) => (
-              <InvoiceList
-                key={item.id}
-                productName={item.product_name}
-                price={item.price}
-                quantity={item.quantity}
-                price_total={item.price_total}
-                currency={invoice.currency}
-              />
-            ))}
-          </tbody>
-        </table>
-      </div>
-    </div>
+      <style jsx>
+        {`
+          .row.items table {
+            width: 100%;
+          }
+        `}
+      </style>
+    </>
   )
 }

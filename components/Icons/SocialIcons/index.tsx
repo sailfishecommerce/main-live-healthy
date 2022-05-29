b2d4f1261ad2559c3ed38afd5aca6c1b55fc9ea3
BisiDavi <oludavidconnect@gmail.com>
2022-05-30 00:42:27 +0100

    social icons updated

--- components/Icons/SocialIcons/index.tsx
+++ components/Icons/SocialIcons/index.tsx
@@ -2,16 +2,39 @@ import FacebookIcon from '@/components/Icons/FacebookIcon'
 import InstagramIcon from '@/components/Icons/InstagramIcon'
 import PinterestIcon from '@/components/Icons/PinterestIcon'
 import TwitterIcon from '@/components/Icons/TwitterIcon'
-import WhatsappIcon from '@/components/Icons/WhatsappIcon'
 
 export default function SocialIcons() {
   return (
     <div className="social-icon-group flex items-center justify-between w-3/4 xl:w-1/2">
-      <WhatsappIcon />
-      <FacebookIcon />
-      <TwitterIcon />
-      <InstagramIcon />
-      <PinterestIcon />
+      <a
+        target="_blank"
+        rel="noreferrer"
+        className="facebook"
+        href="https://web.facebook.com/LiveHealthyHQ?_rdc=1&_rdr"
+      >
+        <FacebookIcon />
+      </a>
+      <a
+        target="_blank"
+        rel="noreferrer"
+        href="https://twitter.com/livehealthyhq"
+      >
+        <TwitterIcon />
+      </a>
+      <a
+        target="_blank"
+        rel="noreferrer"
+        href="https://www.instagram.com/livehealthyhq"
+      >
+        <InstagramIcon />
+      </a>
+      <a
+        target="_blank"
+        rel="noreferrer"
+        href="https://in.pinterest.com/LiveHealthyHQ"
+      >
+        <PinterestIcon />
+      </a>
     </div>
   )
 }

54811aca2e9c7e2baae39a641a23d1a02dd2ac6a
BisiDavi <oludavidconnect@gmail.com>
2022-05-30 01:54:33 +0100

    error fixed

--- components/Invoice/index.tsx
+++ components/Invoice/index.tsx
@@ -1,22 +1,14 @@
-import { PDFDownloadLink, pdf } from '@react-pdf/renderer'
-import { saveAs } from 'file-saver'
+import { PDFDownloadLink } from '@react-pdf/renderer'
 import { BiDownload } from 'react-icons/bi'
 
 import InvoicePage from '@/components/Invoice/InvoicePage'
 import InvoicePdf from '@/components/Invoice/InvoicePdf'
 
-export function DownloadButton({ invoice }: any) {
+export function DownloadButton() {
   return (
     <button
       type="button"
       className="flex  mt-4 items-center bg-mountain-green text-white py-1 p-2 rounded-md"
-      onClick={async () => {
-        const doc = <InvoicePdf invoice={invoice} />
-        const asPdf = pdf([]) // {} is important, throws without an argument
-        asPdf.updateContainer([doc, doc, doc])
-        const blob = await asPdf.toBlob()
-        saveAs(blob, `invoice-${invoice.number}`)
-      }}
     >
       <BiDownload className="mr-2" size={24} />
       Download

a338ae84697727728fd152a42ea7ac03dd6b316a
BisiDavi <oludavidconnect@gmail.com>
2022-06-02 12:08:32 +0100

    mobile view updated

--- components/Header/HeaderBanner.tsx
+++ components/Header/HeaderBanner.tsx
@@ -1,12 +1,18 @@
 import Image from 'next/image'
 
+import { useMediaQuery } from '@/hooks'
+
 export default function HeaderBanner() {
+  const mobile = useMediaQuery('(max-width:768px)')
+  const imageHeight = mobile ? 30 : 45
+  const imageWidth = mobile ? 400 : 1440
+
   return (
     <div className="w-full">
       <Image
         src="/promo-banner.gif"
-        height={45}
-        width={1440}
+        height={imageHeight}
+        width={imageWidth}
         alt="store-banner"
         layout="responsive"
       />

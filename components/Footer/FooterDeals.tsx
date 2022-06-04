3ac04b38130ae85431e0accc0e7d12335596f31d
BisiDavi <oludavidconnect@gmail.com>
2022-06-04 10:39:30 +0100

    contact us form updated

--- components/Footer/FooterDeals.tsx
+++ components/Footer/FooterDeals.tsx
@@ -1,42 +1,47 @@
-import { useState } from 'react'
+import { Formik } from 'formik'
 
-import useMutationAction from '@/hooks/useMutationAction'
+import useVboutMutation from '@/hooks/useVboutMutation'
 
 export default function FooterDeals() {
-  const [newsletterEmail, setNewsletterEmail] = useState<any | null>(null)
-  const { useAddEmailToNewsletter } = useMutationAction()
+  const { useAddEmailToNewsletter } = useVboutMutation()
   const addEmailToNewsletter = useAddEmailToNewsletter()
-
-  function subscribeToNewsletterHandler(e: any) {
-    setNewsletterEmail(e.target.value)
-  }
-
-  function onSubmitHandler(e: any) {
-    e.preventDefault()
-    addEmailToNewsletter.mutate({ email: newsletterEmail, listid: 70390 })
-  }
 
   return (
     <div className="flex flex-col w-full md:mb-4 mb-0 order-1 md:w-1/4">
       <h4 className="font-bold mt-4 mb-2 md:mb-6 text-lg lg:text-xl">
         Get deals in your inbox
       </h4>
-      <form className="deal-form flex items-center" onSubmit={onSubmitHandler}>
-        <input
-          required={true}
-          placeholder="Enter your e-mail"
-          type="email"
-          className="bg-transparent border-b-2 border-black w-full"
-          onChange={subscribeToNewsletterHandler}
-        />
-        <button
-          aria-label="join"
-          type="submit"
-          className="mb-0 border-b-2 border-black"
-        >
-          Join
-        </button>
-      </form>
+      <Formik
+        initialValues={{ email: '' }}
+        onSubmit={(values, { resetForm }) => {
+          addEmailToNewsletter.mutate({ email: values.email, listid: 70390 })
+          resetForm()
+        }}
+      >
+        {(formik) => (
+          <form
+            className="deal-form flex items-center"
+            onSubmit={formik.handleSubmit}
+          >
+            <input
+              required={true}
+              placeholder="Enter your e-mail"
+              type="email"
+              name="email"
+              value={formik.values.email}
+              className="bg-transparent border-b-2 border-black w-full"
+              onChange={formik.handleChange}
+            />
+            <button
+              aria-label="join"
+              type="submit"
+              className="mb-0 border-b-2 border-black"
+            >
+              Join
+            </button>
+          </form>
+        )}
+      </Formik>
     </div>
   )
 }

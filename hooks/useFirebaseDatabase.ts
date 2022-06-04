e5c90b62a722dbe81804c2e660c7fd18f252e1e3
BisiDavi <oludavidconnect@gmail.com>
2022-06-04 22:28:16 +0100

    useFirebase hook updated

--- hooks/useFirebaseDatabase.ts
+++ hooks/useFirebaseDatabase.ts
@@ -6,6 +6,6 @@ import firebaseConfig from '@/lib/firebaseConfig'
 export default function useFirebaseDatabase() {
   const app = initializeApp(firebaseConfig)
   const database = getDatabase(app)
-
-  return
+  console.log('database', database)
+  return null
 }

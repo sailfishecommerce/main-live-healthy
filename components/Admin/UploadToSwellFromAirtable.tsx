a3164629c49ba4844080187282355ce29b884983
BisiDavi <oludavidconnect@gmail.com>
2022-06-13 17:13:11 +0100

    parse csv updated

--- components/Admin/UploadToSwellFromAirtable.tsx
+++ components/Admin/UploadToSwellFromAirtable.tsx
@@ -1,17 +1,102 @@
-import { useCallback } from 'react'
+/* eslint-disable no-nested-ternary */
+/* eslint-disable no-alert */
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-console */
+import { useCallback, useMemo } from 'react'
 import { useDropzone } from 'react-dropzone'
 
 import byteSize from '@/utils/byteSize'
 
+function csvToArray(str, delimiter = ',') {
+  console.log('str', str)
+
+  const headers = str?.slice(0, str.indexOf('\n')).split(delimiter)
+  const rows = str?.slice(str.indexOf('\n') + 1).split('\n')
+  console.log('headers', headers)
+  console.log('rows', rows)
+
+  const arr = rows.map(function (row: any) {
+    const values = row.split(delimiter)
+    const el = headers.reduce(function (
+      objectItem: any,
+      header: string,
+      index: number
+    ) {
+      objectItem[header] = values[index]
+      return objectItem
+    },
+    {})
+    return el
+  })
+  return arr
+}
+
+const baseStyle = {
+  flex: 1,
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  padding: '20px',
+  borderWidth: 2,
+  borderRadius: 2,
+  borderColor: 'white',
+  borderStyle: 'dashed',
+  color: '#9e9aa6',
+  outline: 'none',
+  transition: 'border .24s ease-in-out',
+}
+const focusedStyle = {
+  borderColor: '#2196f3',
+}
+
+const acceptStyle = {
+  borderColor: '#00e676',
+}
+
+const rejectStyle = {
+  borderColor: '#ff1744',
+}
+
 export default function UploadToSwellFromAirtable() {
   const onDrop = useCallback((acceptedFiles) => {
     // Do something with the files
-    console.log('acceptedFiles', acceptedFiles)
+    const csvFile = acceptedFiles[0]
+    console.log('csvFile', csvFile)
+    // const reader = new FileReader()
+    // reader.onload = function (event: any) {
+    //   const text = event.target.value
+    //   const data = csvToArray(text)
+    //   window.alert(JSON.stringify(data))
+    // }
+    // reader.readAsText(csvFile)
   }, [])
-  const { getRootProps, acceptedFiles, getInputProps, isDragActive } =
-    useDropzone({
-      onDrop,
-    })
+
+  const {
+    getRootProps,
+    acceptedFiles,
+    getInputProps,
+    isDragActive,
+    isFocused,
+    isDragAccept,
+    isDragReject,
+  } = useDropzone({
+    onDrop,
+    accept: {
+      '.csv': [
+        '.csv, text/csv, application/vnd.ms-excel, application/csv, text/x-csv, application/x-csv, text/comma-separated-values, text/x-comma-separated-values',
+      ],
+    },
+  })
+
+  const style: any = useMemo(
+    () => ({
+      ...baseStyle,
+      ...(isFocused ? focusedStyle : {}),
+      ...(isDragAccept ? acceptStyle : {}),
+      ...(isDragReject ? rejectStyle : {}),
+    }),
+    [isFocused, isDragAccept, isDragReject]
+  )
   const files = acceptedFiles.map((file: any) => (
     <li key={file.path}>
       <span className="font-bold">{file.name}</span>
@@ -19,6 +104,12 @@ export default function UploadToSwellFromAirtable() {
     </li>
   ))
 
+  console.log(
+    'isFocused, isDragAccept, isDragReject ',
+    isFocused,
+    isDragAccept,
+    isDragReject
+  )
   return (
     <div>
       <h1 className="text-center text-2xl">
@@ -29,7 +120,7 @@ export default function UploadToSwellFromAirtable() {
       </p>
       <div className="upload">
         <div className="upload-area">
-          <div {...getRootProps()}>
+          <div {...getRootProps({ style })}>
             <input
               className="border border-2 h-20 bg-red-500"
               {...getInputProps()}
@@ -51,15 +142,30 @@ export default function UploadToSwellFromAirtable() {
           .upload-area {
             height: 100px;
             width: 60%;
-            background-color: var(--tan-light-hide);
+            background-color: ${isDragActive
+              ? 'var(--tan-hide)'
+              : 'var(--tan-light-hide)'};
             padding: 10px;
             margin-top: 10px;
           }
-          .upload-area input,
           .upload-area div {
             height: 100%;
             width: 100%;
             cursor: pointer;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            text-align: center;
+            margin: auto;
+            justify-content: center;
+            font-size: 16px;
+            font-style: italic;
+            font-weight: 600;
+          }
+          @media (max-width: 1000px) {
+            .upload-area {
+              width: 100%;
+            }
           }
         `}
       </style>

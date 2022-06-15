63d09e3a68a4cb05b93dadb0b9d443ab98d3ae0e
BisiDavi <oludavidconnect@gmail.com>
2022-06-15 12:08:48 +0100

    database content formatted

--- pages/customer-care/shipping-and-delivery.tsx
+++ pages/customer-care/shipping-and-delivery.tsx
@@ -1,6 +1,8 @@
+/* eslint-disable no-console */
+import { convertFromRaw, convertToRaw } from 'draft-js'
 import { initializeApp } from 'firebase/app'
 import { getDatabase, ref, onValue } from 'firebase/database'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 import CustomercareView from '@/components/View/CustomercareView'
 import JsonContent from '@/json/customer-care.json'
@@ -8,8 +10,11 @@ import CustomercareLayout from '@/layouts/customer-care-layout'
 import firebaseConfig from '@/lib/firebaseConfig'
 
 export default function ShippingDeliveriesPage() {
+  const [content, setContent] = useState(null)
   const pageContent = JsonContent.content[3]
   initializeApp(firebaseConfig)
+
+  console.log('content', content)
 
   useEffect(() => {
     const db = getDatabase()
@@ -17,6 +22,10 @@ export default function ShippingDeliveriesPage() {
     onValue(dbRef, (snapshot) => {
       const data = snapshot.val()
       console.log('read-data', data)
+      const contentState = convertFromRaw(JSON.parse(data))
+      const contentStateRaw = convertToRaw(contentState)
+      console.log('contentStateRaw', contentStateRaw)
+      setContent(contentStateRaw)
     })
   }, [])
 

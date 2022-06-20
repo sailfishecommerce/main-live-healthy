937a5bc23eccedd7adf2b32018c1e024c2ff5d6d
BisiDavi <oludavidconnect@gmail.com>
2022-06-20 21:11:29 +0100

    site colors updated

--- hooks/useColorPicker.ts
+++ hooks/useColorPicker.ts
@@ -32,7 +32,7 @@ export default function useColorPicker() {
     } else {
       siteColorsFromDB()
     }
-  }, [siteColors])
+  }, [])
 
   function saveDefaultCodeToDBOnce() {
     const { writeData } = firebaseDatabase()

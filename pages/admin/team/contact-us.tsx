6fc5ab9bf6fc3f1bc147bd2cba524c3f23b12fa0
BisiDavi <oludavidconnect@gmail.com>
2022-06-22 21:40:31 +0100

    admin contact us page updated

--- pages/admin/team/contact-us.tsx
+++ pages/admin/team/contact-us.tsx
@@ -1,3 +1,4 @@
+import AdminContactusBlock from '@/components/Contactus/AdminContactusBlock'
 import DashboardMainView from '@/components/Dashboard/DashboardMainView'
 import DashboardLayout from '@/layouts/dashboard-layout'
 
@@ -12,44 +13,48 @@ export default function ContactusPage() {
           <div className="main-address mb-4">
             <h6 className="text-lg font-semibold">🏢 Main Store Address </h6>
             <div className="address">
-              <p>Live Healthy HK</p>
-              <p>10/F, Cheung Hing Industrial Building</p>
-              <p>12P Smithfield, Kennedy Town</p>
-              <p>Hong Kong Island, Hong Kong.</p>
+              <AdminContactusBlock>
+                <p>Live Healthy HK</p>
+                <p>10/F, Cheung Hing Industrial Building</p>
+                <p>12P Smithfield, Kennedy Town</p>
+                <p>Hong Kong Island, Hong Kong.</p>
+              </AdminContactusBlock>
             </div>
           </div>
           <div className="Working hours mb-4">
             <h6 className="text-lg font-semibold">⏰ Working hours</h6>
             <div className="address">
-              <p>Mon-Fri: 10AM - 7PM</p>
-              <p>Saturday: 11AM - 5PM</p>
+              <AdminContactusBlock>
+                <p>Mon-Fri: 10AM - 7PM</p>
+                <p>Saturday: 11AM - 5PM</p>
+              </AdminContactusBlock>
             </div>
           </div>
           <div className="Phone numbers">
             <h6 className="text-lg font-semibold">📞 Phone numbers</h6>
-            <ul className="address">
-              <li className="flex items-center">
+            <AdminContactusBlock>
+              <div className="flex items-center">
                 <span className="mr-2 font-semibold">For Customers</span>
                 <p>+85370735292</p>
-              </li>
-              <li className="flex items-center">
+              </div>
+              <div className="flex items-center">
                 <span className="mr-2 font-semibold">For Emergency</span>
                 <p>+85370735292</p>
-              </li>
-            </ul>
+              </div>
+            </AdminContactusBlock>
           </div>
           <div className="Email Address">
             <h6 className="text-lg font-semibold">📧 Emails</h6>
-            <ul className="address-list">
-              <li className="flex items-center">
+            <AdminContactusBlock>
+              <div className="flex items-center">
                 <span className="mr-2 font-semibold">For Customers</span>
                 <p>care@livehealthy.com.hk</p>
-              </li>
-              <li className="flex items-center">
+              </div>
+              <div className="flex items-center">
                 <span className="mr-2 font-semibold">For vacancy</span>
                 <p>vacancy@livehealthy.com.hk</p>
-              </li>
-            </ul>
+              </div>
+            </AdminContactusBlock>
           </div>
         </div>
       </DashboardMainView>

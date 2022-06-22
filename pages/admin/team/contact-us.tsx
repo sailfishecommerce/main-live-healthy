482c891456a0401336c113b8bfa8ac06199329d1
BisiDavi <oludavidconnect@gmail.com>
2022-06-22 23:08:10 +0100

    contact us page updated

--- pages/admin/team/contact-us.tsx
+++ pages/admin/team/contact-us.tsx
@@ -1,23 +1,39 @@
-import { useState, useRef } from 'react'
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable no-nested-ternary */
+import { useState, useRef, useEffect } from 'react'
 import ContentEditable from 'react-contenteditable'
 import { AiFillSave } from 'react-icons/ai'
 
 import DashboardMainView from '@/components/Dashboard/DashboardMainView'
+import SpinnerRipple from '@/components/Loader/SpinnerLoader'
 import { useToast } from '@/hooks'
-import content from '@/json/contact-content.json'
 import DashboardLayout from '@/layouts/dashboard-layout'
 import firebaseDatabase from '@/lib/firebaseDatabase'
 
 export default function ContactusPage() {
-  const [contactusState, setContactusState] = useState(content)
+  const [contactusState, setContactusState] = useState<any>(null)
+  const [loading, setLoading] = useState(false)
+
   const { loadingToast, updateToast } = useToast()
 
+  const dbRef = 'articles/team/contact-us/content'
   const toastID = useRef(null)
+
+  useEffect(() => {
+    if (contactusState === null) {
+      readData()
+    }
+  }, [])
+
+  function readData() {
+    const { readFromDB } = firebaseDatabase()
+    readFromDB(dbRef, setContactusState, setLoading)
+  }
 
   function saveChange() {
     loadingToast(toastID)
     const { writeData } = firebaseDatabase()
-    writeData('articles/team/contact-us/content', contactusState)
+    writeData(dbRef, contactusState)
       .then(() => updateToast(toastID, 'success', 'changes saved'))
       .catch(() => updateToast(toastID, 'error', 'an error occured, try again'))
   }
@@ -37,64 +53,71 @@ export default function ContactusPage() {
         <p className="text-lg text-center mt-2 font-thin">
           Click on the text to edit content
         </p>
-        <div className="content grid grid-cols-2 mt-12">
-          <div className="main-address mb-4">
-            <h6 className="text-lg font-semibold">🏢 Main Store Address </h6>
-            <div className="address">
-              <ContentEditable
-                html={contactusState.mainStore}
-                onChange={(e) => handleChange(e, 'mainStore')}
-              />
+
+        {loading ? (
+          <SpinnerRipple centerRipple />
+        ) : contactusState !== null ? (
+          <div className="content grid grid-cols-2 mt-12">
+            <div className="main-address mb-4">
+              <h6 className="text-lg font-semibold">🏢 Main Store Address </h6>
+              <div className="address">
+                <ContentEditable
+                  html={contactusState.mainStore}
+                  onChange={(e) => handleChange(e, 'mainStore')}
+                />
+              </div>
             </div>
-          </div>
-          <div className="Working hours mb-4">
-            <h6 className="text-lg font-semibold">⏰ Working hours</h6>
-            <div className="address">
-              <ContentEditable
-                html={contactusState.workingHours}
-                onChange={(e) => handleChange(e, 'workingHours')}
-              />
+            <div className="Working hours mb-4">
+              <h6 className="text-lg font-semibold">⏰ Working hours</h6>
+              <div className="address">
+                <ContentEditable
+                  html={contactusState.workingHours}
+                  onChange={(e) => handleChange(e, 'workingHours')}
+                />
+              </div>
             </div>
-          </div>
-          <div className="Phone numbers">
-            <h6 className="text-lg font-semibold">📞 Phone numbers</h6>
-            <div className="address">
-              <ContentEditable
-                html={contactusState.phoneNumbers}
-                onChange={(e) => handleChange(e, 'phoneNumbers')}
-              />
+            <div className="Phone numbers">
+              <h6 className="text-lg font-semibold">📞 Phone numbers</h6>
+              <div className="address">
+                <ContentEditable
+                  html={contactusState.phoneNumbers}
+                  onChange={(e) => handleChange(e, 'phoneNumbers')}
+                />
+              </div>
             </div>
-          </div>
-          <div className="Email Address">
-            <h6 className="text-lg font-semibold">📧 Emails</h6>
-            <div className="address">
-              <ContentEditable
-                html={contactusState.emails}
-                onChange={(e) => handleChange(e, 'emails')}
-              />
+            <div className="Email Address">
+              <h6 className="text-lg font-semibold">📧 Emails</h6>
+              <div className="address">
+                <ContentEditable
+                  html={contactusState.emails}
+                  onChange={(e) => handleChange(e, 'emails')}
+                />
+              </div>
             </div>
+            <button
+              type="button"
+              className="bg-mountain-green text-white saveChanges"
+              onClick={saveChange}
+            >
+              <AiFillSave className="mr-2" size={20} /> Save Changes
+            </button>
+            <style jsx>{`
+              .address {
+                width: 60%;
+              }
+              .saveChanges {
+                width: 150px;
+                height: 40px;
+                margin-top: 20px;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+              }
+            `}</style>
           </div>
-          <button
-            type="button"
-            className="bg-mountain-green text-white saveChanges"
-            onClick={saveChange}
-          >
-            <AiFillSave className="mr-2" size={20} /> Save Changes
-          </button>
-          <style jsx>{`
-            .address {
-              width: 60%;
-            }
-            .saveChanges {
-              width: 150px;
-              height: 40px;
-              margin-top: 20px;
-              display: flex;
-              align-items: center;
-              justify-content: center;
-            }
-          `}</style>
-        </div>
+        ) : (
+          <p>Unable to fetch data</p>
+        )}
       </DashboardMainView>
     </DashboardLayout>
   )

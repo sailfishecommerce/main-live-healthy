416a8147d6f3d5e2d7bf3213b36a5ea3de2d0799
BisiDavi <oludavidconnect@gmail.com>
2022-06-28 16:40:31 +0100

    blog post updated

--- components/Blog/BlogArticle.tsx
+++ components/Blog/BlogArticle.tsx
@@ -2,16 +2,21 @@ import BlogAuthor from '@/components/Blog/BlogAuthor'
 import BlogContent from '@/components/Blog/BlogContent'
 import ShareBlogLink from '@/components/Blog/ShareBlogLink'
 import Breadcrumb from '@/components/Breadcrumb'
-import breadcrumb from '@/json/breadcrumb.json'
 
 export default function BlogArticle({ postContent, blogPost }: any) {
   const title = JSON.parse(blogPost[1].title)
   const author = JSON.parse(blogPost[1].author)
   const postDate = JSON.parse(blogPost[1].createdAt)
 
+  const blogbreadCrumbArray = [
+    { name: 'Homepage', link: '/' },
+    { name: 'Blog', link: '/blog' },
+    { name: title, link: null, active: true },
+  ]
+
   return (
     <article className="lg:w-3/4 w-full lg:pr-12 px-4">
-      <Breadcrumb breadcrumbItems={breadcrumb.blog} />
+      <Breadcrumb breadcrumbItems={blogbreadCrumbArray} />
       <h1 className="blog-title text-3xl mb-8 font-bold">{title}</h1>
       <div className="row-1 items-center flex flex-col">
         <ShareBlogLink author={author} postDate={postDate} />
--- components/Blog/BlogContent.tsx
+++ components/Blog/BlogContent.tsx
@@ -1,10 +1,8 @@
 import Image from 'next/image'
 
 import displayTextView from '@/components/View/displayTextView'
-import blogContent from '@/json/blog.json'
 
 export default function BlogContent({ post }: any) {
-  const content = blogContent[0]
   const blogPostImages: any[] = Object.entries(post.entityMap)
   const mainImage = blogPostImages[0][1].data.src
 
@@ -15,7 +13,7 @@ export default function BlogContent({ post }: any) {
           src={mainImage}
           height={250}
           width={500}
-          alt={content.title}
+          alt="post"
           className="rounded-xl mb-8"
           layout="responsive"
         />

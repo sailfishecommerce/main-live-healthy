93c07dbb825695719df946a257da8195c6c685eb
BisiDavi <oludavidconnect@gmail.com>
2022-07-11 01:58:40 +0100

    banner gif updated

--- components/Header/HeaderBanner.tsx
+++ components/Header/HeaderBanner.tsx
@@ -1,24 +1,16 @@
-import Image from 'next/image'
-
 import { useMediaQuery } from '@/hooks'
 
 export default function HeaderBanner() {
   const mobile = useMediaQuery('(max-width:700px)')
-  const imageHeight = mobile ? 60 : 45
-  const imageWidth = mobile ? 450 : 1440
 
-  const imageSrc = mobile ? '/mobile-promo-banner-gif.gif' : '/promo-banner.gif'
+  const videoSrc = mobile ? '/mobile-promo-banner.mp4' : '/promo-banner.mp4'
 
   return (
     <div className="w-full">
-      <Image
-        src={imageSrc}
-        height={imageHeight}
-        width={imageWidth}
-        alt="store-banner"
-        layout="responsive"
-        priority={true}
-      />
+      <video autoPlay loop muted playsInline width="100%">
+        <source src={videoSrc} type="video/webm" />
+        <source src={videoSrc} type="video/mp4" />
+      </video>
     </div>
   )
 }

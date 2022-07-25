e58efa99b74bac5a31e75fdbbf6cbfe24e289531
BisiDavi <oludavidconnect@gmail.com>
2022-07-25 07:01:49 +0100

    error boundary fixed

--- layouts/app-layout.tsx
+++ layouts/app-layout.tsx
@@ -1,9 +1,12 @@
+import { useAtom } from 'jotai'
 import dynamic from 'next/dynamic'
 import Head from 'next/head'
 import type { PropsWithChildren } from 'react'
+import { useEffect } from 'react'
 
 import Header from '@/components/Header'
 import DefaultLayout from '@/layouts/default-layout'
+import { reloadPageAtom } from '@/lib/atomConfig'
 
 const Footer = dynamic(
   () => import(/* webpackChunkName: 'Footer' */ '@/components/Footer'),
@@ -20,6 +23,14 @@ export default function Applayout({
   children,
   title,
 }: PropsWithChildren<Props>) {
+  const [reloadPage, setReloadPage] = useAtom(reloadPageAtom)
+
+  useEffect(() => {
+    if (reloadPage) {
+      setReloadPage(false)
+    }
+  }, [])
+
   return (
     <DefaultLayout>
       <Head>

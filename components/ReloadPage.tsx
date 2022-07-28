cfea0d17c337c6888c7ce1fdd3e69fbdc5391002
BisiDavi <oludavidconnect@gmail.com>
2022-07-28 16:25:03 +0100

    reload page updated

--- components/ReloadPage.tsx
+++ components/ReloadPage.tsx
@@ -1,9 +1,9 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useAtom } from 'jotai'
-import Image from 'next/image'
 import { useRouter } from 'next/router'
 import { useEffect } from 'react'
 
+import SpinnerRipple from '@/components/Loader/SpinnerLoader'
 import { reloadPageAtom } from '@/lib/atomConfig'
 
 export default function ReloadPage() {
@@ -20,7 +20,8 @@ export default function ReloadPage() {
   return (
     <div className="w-full">
       <div className="container mx-auto h-96 justify-center flex items-center">
-        <div className="w-1/2 bg-gray-150 flex items-center justify-center h-full">
+        <SpinnerRipple centerRipple />
+        {/* <div className="w-1/2 bg-gray-150 flex items-center justify-center h-full">
           <h1 className="font-bold text-xl">Please refresh this page </h1>
         </div>
         <div className="w-1/2">
@@ -30,7 +31,7 @@ export default function ReloadPage() {
             width={750}
             alt="refresh page"
           />
-        </div>
+        </div> */}
       </div>
     </div>
   )

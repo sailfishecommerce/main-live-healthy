3173ff325ac3d857d40e423627a4ad00168a3556
BisiDavi <oludavidconnect@gmail.com>
2022-07-29 13:20:11 +0100

    product banner updated

--- components/Cards/ProductBannerCard.tsx
+++ components/Cards/ProductBannerCard.tsx
@@ -16,14 +16,17 @@ export default function ProductBannerCard({ product, color }: any) {
       : product.images[0].file.url
 
   return (
-    <div className="hover:bg-white flex ml-4 p-2 relative hover:shadow-lg product hover:rounded-lg product hover:border">
-      <DiscountTag price={product.price} salePrice={product.sale_price} />
-
-      <Link
-        passHref
-        href={`/product/${product.slug}?queryID=${product.__queryID}`}
+    <Link
+      passHref
+      href={`/product/${product.slug}?queryID=${product.__queryID}`}
+    >
+      <a
+        title={product.name}
+        className="hover:bg-white flex ml-4 p-2 relative hover:shadow-lg product hover:rounded-lg product hover:border"
       >
-        <a title={product.name} className="w-2/5 image-wrapper">
+        <DiscountTag price={product.price} salePrice={product.sale_price} />
+
+        <span className="w-2/5 block image-wrapper">
           <Image
             src={productImage}
             alt={product.name}
@@ -32,54 +35,54 @@ export default function ProductBannerCard({ product, color }: any) {
             blurDataURL={productImage}
             layout="responsive"
           />
-        </a>
-      </Link>
-      <div className="w-3/5 pl-2 text">
-        <h4 className="vendor text-xs md:text-md font-bold pl-2 my-0 py-0 mb-1 md:mb-0 md:h-5">
-          {product.vendor}
-        </h4>
-        <div className="product-name-view xl:mb-6 mb-2">
-          <h3 className="text-xs md:text-md product-name">{product.name}</h3>
-        </div>
-        <div className="price-group md:flex-row flex-col items-start flex xl:items-center justify-between">
-          <FormattedPrice
-            price={product.sale_price}
-            className="text-sm md:text-md text-black mb-1 xl:mb-0 font-semibold"
-          />
-          {product.price !== 0 && (
+        </span>
+        <div className="w-3/5 pl-2 text">
+          <h4 className="vendor text-xs md:text-md font-bold pl-2 my-0 py-0 mb-1 md:mb-0 md:h-5">
+            {product.vendor}
+          </h4>
+          <div className="product-name-view xl:mb-6 mb-2">
+            <h3 className="text-xs md:text-md product-name">{product.name}</h3>
+          </div>
+          <div className="price-group md:flex-row flex-col items-start flex xl:items-center justify-between">
             <FormattedPrice
-              price={product.price}
-              className="text-sm strike-through md:text-sm text-red-500 font-semibold"
+              price={product.sale_price}
+              className="text-sm md:text-md text-black mb-1 xl:mb-0 font-semibold"
             />
-          )}
+            {product.price !== 0 && (
+              <FormattedPrice
+                price={product.price}
+                className="text-sm strike-through md:text-sm text-red-500 font-semibold"
+              />
+            )}
+          </div>
+          <button
+            type="button"
+            className="bg-mountain-green mt-4 w-full w-3/4 xl:w-4/5 justify-center h-8 text-white xl:px-4 py-1 flex items-center mx-auto rounded-md"
+            onClick={addToCartHandler}
+          >
+            <CartIcon />
+            <p className="text-xs md:text-sm">Add to cart</p>
+          </button>
         </div>
-        <button
-          type="button"
-          className="bg-mountain-green mt-4 w-full w-3/4 xl:w-4/5 justify-center h-8 text-white xl:px-4 py-1 flex items-center mx-auto rounded-md"
-          onClick={addToCartHandler}
-        >
-          <CartIcon />
-          <p className="text-xs md:text-sm">Add to cart</p>
-        </button>
-      </div>
-      <style jsx>
-        {`
-          .product {
-            font-family: 'Commissioner', sans-serif;
-            font-display: swap;
-          }
-          .vendor {
-            border-left: 3px solid ${color};
-            color: ${color};
-          }
-          @media (max-width: 500px) {
-            a.product {
-              width: 90%;
-              margin: 0px;
+        <style jsx>
+          {`
+            .product {
+              font-family: 'Commissioner', sans-serif;
+              font-display: swap;
             }
-          }
-        `}
-      </style>
-    </div>
+            .vendor {
+              border-left: 3px solid ${color};
+              color: ${color};
+            }
+            @media (max-width: 500px) {
+              a.product {
+                width: 90%;
+                margin: 0px;
+              }
+            }
+          `}
+        </style>
+      </a>
+    </Link>
   )
 }
